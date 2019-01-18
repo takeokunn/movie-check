4833e7f67198229d25959827c1793124632a3a5c
takeokunn <bararararatty@gmail.com>
2019-01-18 17:04:35 +0900

    Refactor codes

--- index.js
+++ index.js
@@ -1,64 +1,54 @@
 const fs = require('fs')
-var textLine = fs.readFileSync('data/test_id.csv', 'utf8', function (err, data) {
-  if (err) throw err;
-});
-// console.log(textLine);
-textArray = textLine.split(/\r\n|\r|\n/);
-// console.log(textArray);
+const puppeteer = require('puppeteer');
 
-function delay(timeout) {
-  return new Promise((resolve) => {
-    setTimeout(resolve, timeout);
-  });
-}
+const video = require('./video.json');
+
+const sleep = time => new Promise(resolve => setTimeout(resolve, time));
 
 const pc = {
-  'name': 'Chrome Mac',
-  'userAgent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36',
-  'viewport': {
-      'width': 1200,
-      'height': 800,
-      'deviceScaleFactor': 1,
-      'isMobile': false,
-      'hasTouch': false,
-      'isLandscape': false
-  }
+    'name': 'Chrome Mac',
+    'userAgent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36',
+    'viewport': {
+        'width': 1200,
+        'height': 800,
+        'deviceScaleFactor': 1,
+        'isMobile': false,
+        'hasTouch': false,
+        'isLandscape': false
+    }
 };
 
-const puppeteer = require('puppeteer');
-(async () => {
-  const browser = await puppeteer.launch({
-    headless: false,
-    slowMo: 100
-  })
-  const page = await browser.newPage()
-  await page.emulate(pc);
+const play_video = async (page, video_id) => {
+    const url = `https://www.youtube.com/watch?v=${video_id}`;
 
-  for (let i = 0; i < textArray.length; i++) {
-    const movie_id = textArray[i]
-    console.log("movie_id:" + movie_id)
+    await page.goto(url, { waitUntil: 'networkidle2' });
+    await sleep(3000);
 
-    // console.log("Page Goto: https://www.youtube.com/watch?v=" + movie_id)
-    await page.goto('https://www.youtube.com/watch?v=' + movie_id, {
-      waitUntil: 'networkidle2'
-    })
+    try {
+        // open menu
+        await page.waitForSelector('#menu > .ytd-video-primary-info-renderer > #button > #button > .style-scope')
+        await page.click('#menu > .ytd-video-primary-info-renderer > #button > #button > .style-scope')
 
-    // delay delay delay
-    await delay(3000);
+        // open transcript
+        await page.waitForSelector('#contentWrapper > .dropdown-content > #items > .style-scope:nth-child(2)')
+        await page.click('#contentWrapper > .dropdown-content > #items > .style-scope:nth-child(2)')
 
-    // open menu
-    await page.waitForSelector('#menu > .ytd-video-primary-info-renderer > #button > #button > .style-scope')
-    await page.click('#menu > .ytd-video-primary-info-renderer > #button > #button > .style-scope')
+        // get transcript
+        await page.waitForSelector('.ytd-transcript-body-renderer')
 
-    // open transcript
-    await page.waitForSelector('#contentWrapper > .dropdown-content > #items > .style-scope:nth-child(2) > .style-scope:nth-child(2)')
-    await page.click('#contentWrapper > .dropdown-content > #items > .style-scope:nth-child(2) > .style-scope:nth-child(2)')
+        const transcript = await page.evaluate(() => document.querySelector('#body .ytd-transcript-renderer').innerText);
+        console.log(transcript);
+    } catch (e) {
+        console.log(e);
+        console.log("error: " + video_id);
+    }
+};
 
-    // get transcript
-    await page.waitForSelector('.ytd-transcript-body-renderer')
-    const transcript = await page.evaluate(() => document.querySelector('#body .ytd-transcript-renderer').innerText);
-    console.log(transcript)
+const main = async () => {
+    const browser = await puppeteer.launch({ headless: false, slowMo: 100, args: ['--lang=ja,en-US,en'] });
+    const page = await browser.newPage()
+    await page.emulate(pc);
+    await video.ids.map(id => play_video(page, id));
+}
 
-  }
-  await browser.close()
-})()
+main();
